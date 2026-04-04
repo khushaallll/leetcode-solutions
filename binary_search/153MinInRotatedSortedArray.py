2f0a833461da9f155fb94f7676cb5149e5608328
khushaallll <kolikhushal18@gmail.com>
2026-04-04 18:34:16 +0100

    minor change in mininrotatedarrat

--- binary_search/153MinInRotatedSortedArray.py
+++ binary_search/153MinInRotatedSortedArray.py
@@ -2,7 +2,7 @@ def findMin(nums: list[int]) -> int:
     left = 0
     right = len(nums) - 1
 
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
         if nums[mid] > nums[right]:
             left = mid + 1
@@ -10,11 +10,10 @@ def findMin(nums: list[int]) -> int:
         elif nums[mid] <= nums[right]:
             right = mid
         
-        if mid == left == right:
-            return nums[mid]
+    return nums[left]
     
 
-# nums = [2,1]
+nums = [2,1]
 nums=[1,2,3,4]
-# nums = [4, 5, 6, 7, 0, 1, 2]
+nums = [4, 5, 6, 7, 0, 1, 2]
 print(findMin(nums))
